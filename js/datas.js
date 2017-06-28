c7f5c583a02f2f26fa5628943b1d261899716e5e
emoc11 <emoc11@free.fr>
2017-06-28 19:30:48 +0200

    update for v2

---
+++ js/datas.js
@@ -0,0 +1,888 @@
+var datas =
+[
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "actu"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "social"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+	{
+		"type": "ambassadeur"
+	},
+
+]
--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ $(function() {
 	// FLARE ATOUR DE LA PLANETE
 
 	// Animation variables
-	var EARTH_ROTATE = true;
+	var SPHERE_ROTATE = true;
+	// var WAIT_BEFORE_ROTATE = 0;
+	// var WAIT_BEFORE_ROTATE_TIME = 200;
 
 	// Other variables
 	var clicableObjects = [];
@@ -26,7 +28,7 @@ $(function() {
 
 	// Get the DOM element to attach to
 	var container =
-	    document.querySelector('#earth');
+	    document.querySelector('#sphere');
 
 	// Create a WebGL renderer, camera
 	// and a scene
@@ -52,49 +54,29 @@ $(function() {
 	container.appendChild(renderer.domElement);
 
 	// Set up the sphere vars
-	var RADIUS = 100;
-	var SEGMENTS = 32;
-	var RINGS = 32;
+	var RADIUS = 50;
+	var SEGMENTS = 8;
+	var RINGS = 8;
+	var DETAILS = 2;
 
 	// Create a new mesh with
 	// sphere geometry
 	var textureLoader = new THREE.TextureLoader();
-	earth = new THREE.Mesh(
-		new THREE.SphereGeometry(RADIUS, SEGMENTS, RINGS),
-		new THREE.MeshPhongMaterial({
-			map: textureLoader.load('img/earthmapnasa.jpg'),
-			bumpMap: textureLoader.load('img/earthbump1k.jpg'),
-			bumpScale:   0.05,
-			specularMap: textureLoader.load('img/earthspec1k.jpg'),
-			specular: new THREE.Color('grey'),
-			shininess: 10
-		})
-	);
+	var mainGeom = new THREE.IcosahedronGeometry(RADIUS, DETAILS);
+	var mainMaterial = new THREE.MeshBasicMaterial({ color: 0x000000, transparent: true, opacity: 0.0, depthWrite: false });
+
+	sphere = new THREE.Mesh(mainGeom, mainMaterial);
 
 	// Move the Sphere back in Z so we
 	// can see it.
-	earth.position.z = -300;
-	earth.rotation.x = 15 * Math.PI / 180;
-	earth.rotation.y = 0;
+	sphere.position.z = -190;
+	sphere.rotation.x = 90 * Math.PI / 180;
+	sphere.rotation.y = 0;
 
 	// Mouse orbit control
 
 	// Finally, add the sphere to the scene.
-	scene.add(earth);
-
-	// Create clouds sphere
-	var cloudGeo = new THREE.SphereGeometry(RADIUS+.8, SEGMENTS, RINGS);
-	var cloudMat = new THREE.MeshPhongMaterial({
-			map: textureLoader.load('img/earthclouds.png'),
-			side: THREE.DoubleSide,
-			opacity: 0.8,
-			transparent: true,
-			depthWrite: false
-	});
-	var cloudMesh = new THREE.Mesh(cloudGeo, cloudMat);
-
-	// Add clouds to earth
-	earth.add(cloudMesh);
+	scene.add(sphere);
 
 	// Create Night Sky with stars
 	var skyGeo = new THREE.SphereGeometry(300, 60, 60);
@@ -107,29 +89,15 @@ $(function() {
 	var skyMesh = new THREE.Mesh(skyGeo, skyMat);
 
 	skyMesh.position.z = -400;
-	// Add clouds to earth
+	// Add clouds to sphere
 	scene.add(skyMesh);
 
-	// SUPER SIMPLE GLOW EFFECT
-	// use sprite because it appears the same from all angles
-	var spriteMaterial = new THREE.SpriteMaterial(
-	{
-		map: textureLoader.load( 'img/glow.png' ),
-		color: 0xFFFFFF,
-		transparent: false,
-		blending: THREE.AdditiveBlending
-	});
-	var sprite = new THREE.Sprite( spriteMaterial );
-	sprite.scale.set(245, 245, 1.0);
-	sprite.lookAt(earth.position);
-	earth.add(sprite); // this centers the glow at the mesh
-
 	// Add Light
-	var light = new THREE.AmbientLight( 0xFFFFFF ); // soft white light
+	var light = new THREE.AmbientLight( 0xFFFFFF, .3 );
 	scene.add( light );
 
 
-	var spotLight = new THREE.SpotLight(0xffffff, .4, 400, 10, 2);
+	var spotLight = new THREE.SpotLight(0xffffff, 5, 185, 10, 4);
 	scene.add(spotLight);
 
 	// DRAW !
@@ -140,14 +108,14 @@ $(function() {
 	function update () {
 		requestAnimationFrame(update);
 
-		// Clouds rotation
-		cloudMesh.rotation.y  += 0.0003;
+		sphere.rotation.z += 1/32 * 0.01;
 
-		TWEEN.update();
-
-		if (EARTH_ROTATE) {
-			earth.rotation.y -= 1/32 * 0.05;
-		}
+		// if (WAIT_BEFORE_ROTATE <= 0) {
+		// 	sphere.rotation.z += 1/32 * 0.01;
+		// 	WAIT_BEFORE_ROTATE = 0;
+		// } else if(WAIT_BEFORE_ROTATE > 0) {
+		// 	WAIT_BEFORE_ROTATE -= 1;
+		// }
 
 		render();
 	}
@@ -163,28 +131,13 @@ $(function() {
 		camera.updateProjectionMatrix();
 	});
 
-	function getLatLon(location, callback) {
-		var encodedLocation = location.replace(/\s/g, '+');
-		var httpRequest = new XMLHttpRequest();
-
-		console.log(encodedLocation);
-
-		httpRequest.open('GET', 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodedLocation);
-		httpRequest.send(null);
-		httpRequest.onreadystatechange = function() {
-			if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-				var result = JSON.parse(httpRequest.responseText);
-
-				if (result.results.length > 0) {
-					var latitude = result.results[0].geometry.location.lat;
-					var longitude = result.results[0].geometry.location.lng;
-
-					var res = latitude+","+longitude;
-
-					callback(res);
-				}
-			}
-		};
+	function getPosition() {
+		// Random latitude from -90° to 90°
+		var lat = Math.round(Math.random() * 180 - 90);
+		// Random longitude from -180° to 180°
+		var long = Math.round(Math.random() * 360 - 180);
+		//return both values
+		return lat+","+long;
 	}
 
 	function changeLocalisation(localisation) {
@@ -195,12 +148,12 @@ $(function() {
 		var verticalOffset = 0.1;
 
 		var longToDest = 270 - lon;
-		EARTH_ROTATE = false;
 		rotateOnGoodSide(longToDest);
+		// WAIT_BEFORE_ROTATE = WAIT_BEFORE_ROTATE_TIME;
 	}
 
 	function rotateOnGoodSide( targetDegree ){
-		var currentYRotation = earth.rotation.y * 180 / Math.PI;
+		var currentYRotation = sphere.rotation.y * 180 / Math.PI;
 
 		// Nicely refacto degree to min 0° and max 360°
 		currentYRotation = niceDegree(currentYRotation);
@@ -210,10 +163,10 @@ $(function() {
 
 		if((targetDegree-currentYRotation) > 180) {
 			angleToGo = (360 - (targetDegree-currentYRotation)) * (Math.PI / 180);
-			TweenMax.to(earth.rotation, .3, {y: "-="+angleToGo, ease: Quad.easeOut});
+			TweenMax.to(sphere.rotation, .3, {y: "-="+angleToGo, ease: Quad.easeOut});
 		} else {
 			angleToGo = ((targetDegree-currentYRotation)) * (Math.PI / 180);
-			TweenMax.to(earth.rotation, .3, {y: "+="+angleToGo, ease: Quad.easeOut});
+			TweenMax.to(sphere.rotation, .3, {y: "+="+angleToGo, ease: Quad.easeOut});
 		}
 	}
 
@@ -226,50 +179,161 @@ $(function() {
 		return deg;
 	}
 
-	function convertLatLonToVec3 ( lat, lon ) {
+	function latLongToVec3 ( lat, lon ) {
 		lat =  lat * Math.PI / 180.0;
 		lon = -lon * Math.PI / 180.0;
+
+		// RANDOM ON CIRCLE AND SOME OUTSIDE
+		// var rd = Math.round(Math.random() * 15 - 5);
+		// var displacement = - Math.random();
+		// if(rd == -5) {
+		// 	return new THREE.Vector3(
+		// 		Math.cos(lat) * Math.cos(lon),
+		// 		Math.sin(lat),
+		// 		Math.cos(lat + displacement) * Math.sin(lon));
+		// } else if(rd == -4) {
+		// 	return new THREE.Vector3(
+		// 		Math.cos(lat) * Math.cos(lon),
+		// 		Math.sin(lat),
+		// 		Math.cos(lat) * Math.sin(lon + displacement));
+		// } else if(rd == -3) {
+		// 	return new THREE.Vector3(
+		// 		Math.cos(lat) * Math.cos(lon),
+		// 		Math.sin(lat + displacement),
+		// 		Math.cos(lat) * Math.sin(lon));
+		// } else if(rd == -2) {
+		// 	return new THREE.Vector3(
+		// 		Math.cos(lat) * Math.cos(lon),
+		// 		Math.sin(lat + displacement),
+		// 		Math.cos(lat) * Math.sin(lon));
+		// } else if(rd == -1) {
+		// 	return new THREE.Vector3(
+		// 		Math.cos(lat + displacement) * Math.cos(lon),
+		// 		Math.sin(lat),
+		// 		Math.cos(lat) * Math.sin(lon));
+		// } else if(rd == 0) {
+		// 	return new THREE.Vector3(
+		// 		Math.cos(lat) * Math.cos(lon + displacement),
+		// 		Math.sin(lat),
+		// 		Math.cos(lat) * Math.sin(lon));
+		// } else {
+		// 	return new THREE.Vector3(
+		// 		Math.cos(lat) * Math.cos(lon),
+		// 		Math.sin(lat),
+		// 		Math.cos(lat) * Math.sin(lon));
+		// }
+
 		return new THREE.Vector3(
 			Math.cos(lat) * Math.cos(lon),
 			Math.sin(lat),
 			Math.cos(lat) * Math.sin(lon));
 	}
 
-	function placeMarker(localisation) {
+	function placeMarker(localisation, obj) {
 		var latLon = localisation.split(",");
 		var lat = latLon[0];
 		var lon = latLon[1];
 
-		var latLonPos = convertLatLonToVec3(lat, lon);
-		latLonPos.multiplyScalar(RADIUS-1);
+		var latLonPos = latLongToVec3(lat, lon);
+		latLonPos.multiplyScalar(RADIUS);
+
+		var pinColor = new THREE.Color(obj.color);
 
 		var marker = new THREE.Mesh(
-			new THREE.SphereGeometry(1.4,16,16),
-			new THREE.MeshBasicMaterial({color:0xFF0000})
+			new THREE.SphereGeometry(.5,16,16),
+			new THREE.MeshLambertMaterial({color: pinColor, transparent: true, opacity: 0, depthWrite: false})
 		);
+		marker.type = obj.type;
 
-		marker.position.copy(latLonPos);
-		marker.loc = lat+","+lon;
+		// marker.position.copy(latLonPos);
+		marker.position.x = Math.random() * $(window).innerHeight()/4 - $(window).innerHeight()/8;
+		marker.position.y = Math.random() * $(window).innerHeight()/4 - $(window).innerHeight()/8;
+		marker.position.z = Math.random() * $(window).innerHeight()/4 - $(window).innerHeight()/8;
 
-		earth.add(marker);
+		marker.baseLoc = {};
+		marker.baseLoc.x = marker.position.x;
+		marker.baseLoc.y = marker.position.y;
+		marker.baseLoc.z = marker.position.z;
+
+		marker.sphereLoc = {};
+		marker.sphereLoc.x = latLonPos.x;
+		marker.sphereLoc.y = latLonPos.y;
+		marker.sphereLoc.z = latLonPos.z;
+
+		sphere.add(marker);
+		var speedAnim = Math.random() * 2 + .5;
+		TweenMax.to(marker.position, speedAnim/2, {x: latLonPos.x, y:latLonPos.y, z:latLonPos.z, ease:Quad.easeOut, delay: speedAnim/8});
+		TweenMax.to(marker.material, speedAnim/2, {opacity: 1, ease:Quad.easeOut});
 		clicableObjects.push(marker);
 	}
+	var dataCount = datas.length;
+	$.each(datas, function(i) {
+		var latlong = getPosition();
+		switch(datas[i].type) {
+			case "actu":
+				datas[i].color = "rgb(255,0,0)";
+				break;
+			case "social":
+				datas[i].color = "rgb(0,255,0)";
+				break;
+			case "ambassadeur":
+				datas[i].color = "rgb(0,0,255)";
+				break;
+		}
 
-	$("nav li.city").each(function() {
-		getLatLon($(this).text(), function(latLon) {
-			placeMarker(latLon);
-		});
+		placeMarker(latlong, datas[i]);
 	});
 
-	$("nav li.city").bind("click", function() {
-		getLatLon($(this).text(), function(latLon) {
-			changeLocalisation(latLon);
-		});
+	$("nav li").bind("click", function() {
+		var typeToShow = $(this).attr("data-type");
+
+		filterMarker(typeToShow);
 	});
 
-	$("nav li.backToAutoRotate").bind("click", function() {
-		EARTH_ROTATE = true;
-	});
+	function doOutAnim() {
+		// EXPLOSION ANIM, comme pour apparaitre en sens inverse
+		// $.each(sphere.children, function() {
+		// 	var speedAnim = Math.random() * 2 + .5;
+		// 	TweenMax.to(this.position, speedAnim, {x: this.baseLoc.x, y: this.baseLoc.y, z: this.baseLoc.z, ease:Quad.easeOut});
+		// 	TweenMax.to(this.material, speedAnim/2, {opacity: 0, ease:Quad.easeOut, delay: speedAnim/2});
+		// });
+
+		// SPHERE SCALE + ROTATION -> transition
+		var newScale = 2.8;
+		if(sphere.rotation.x == 90 * Math.PI / 180 && sphere.rotation.y == 0) {
+			TweenMax.to(sphere.scale, .8, {x: newScale, y: newScale, z: newScale, ease:Quad.easeInOut});
+			TweenMax.to(sphere.rotation, .8, {z: "+=2", ease:Quad.easeInOut});
+			$.each(sphere.children, function() {
+				TweenMax.to(this.material, .8/2, {opacity: 0, ease:Quad.easeOut, delay: .8/4});
+			});
+		} else {
+			TweenMax.to(sphere.rotation, .6, {x: 90 * Math.PI / 180, y: 0, ease:Quad.easeInOut});
+			TweenMax.to(sphere.scale, .8, {x: newScale, y: newScale, z: newScale, ease:Quad.easeInOut, delay: .5});
+			TweenMax.to(sphere.rotation, .8, {z: "+=2", ease:Quad.easeInOut, delay: .5});
+			$.each(sphere.children, function() {
+				TweenMax.to(this.material, .8/2, {opacity: 0, ease:Quad.easeOut, delay: .8/4+.5});
+			});
+		}
+
+	}
+
+	function filterMarker(typeToShow) {
+		if(typeToShow != "all" && typeToShow != "outAnim") {
+			$.each(sphere.children, function() {
+				if(this.type != typeToShow) {
+					TweenMax.to(this.material, .2, {opacity: 0, ease:Quad.easeOut});
+				} else {
+					TweenMax.to(this.material, .2, {opacity: 1, ease:Quad.easeOut});
+				}
+			});
+		} else if (typeToShow == "outAnim") {
+			doOutAnim();
+		} else if (typeToShow == "all") {
+			$.each(sphere.children, function() {
+				TweenMax.to(this.material, .2, {opacity: 1, ease:Quad.easeOut});
+			});
+		}
+	}
 
 	var raycaster,mouse;
 
@@ -282,7 +346,7 @@ $(function() {
 		MOUSE_IS_DOWN = true;
 
 		MOUSECLIC_X = event.clientX;
-		MOUSECLIC_Y = event.clientY;$
+		MOUSECLIC_Y = event.clientY;
 
 		mouse.x = ( event.clientX / renderer.domElement.clientWidth ) * 2 - 1;
 		mouse.y = - ( event.clientY / renderer.domElement.clientHeight ) * 2 + 1;
@@ -291,10 +355,12 @@ $(function() {
 		raycaster.setFromCamera( mouse, camera );
 		var intersects = raycaster.intersectObjects( clicableObjects );
 		if ( intersects.length > 0 ) {
+
 			// Si clic sur pin -> direction sur la pin
-			changeLocalisation(intersects[ 0 ].object.loc);
+			// changeLocalisation(intersects[ 0 ].object.loc);
 		}
 	}
+
 	document.addEventListener( 'mouseup', onDocumentMouseUp, false );
 	function onDocumentMouseUp(event) {
 		event.preventDefault();
@@ -308,11 +374,14 @@ $(function() {
 		}
 
 		event.preventDefault();
+		// WAIT_BEFORE_ROTATE = WAIT_BEFORE_ROTATE_TIME;
 		var deltaX = event.clientX - MOUSECLIC_X;
+		var deltaY = event.clientY - MOUSECLIC_Y;
 		MOUSECLIC_X = event.clientX;
 		MOUSECLIC_Y = event.clientY;
 
-		earth.rotation.y += deltaX * Math.PI / 180;
+		sphere.rotation.y += deltaX * Math.PI / 180;
+		sphere.rotation.x += deltaY * Math.PI / 180;
 	}
 
 });
